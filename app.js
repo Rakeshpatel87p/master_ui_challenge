962d2840203de5057b70a1d807204b3197b83317
Rakesh Patel <emailme@rakeshrpatel.com>
2016-10-07 07:59:36 -0400

    updated css

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 $(function() {
     $(document).scroll(function() {
         $('.mainNavigationBar')
-        	.css('position', 'fixed')
+            .css('position', 'fixed')
             .css('padding-bottom', '0')
-        
+
     })
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 })
