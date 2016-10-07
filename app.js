d1992812c11bdb7a2f99c79294bc4f781a55fa38
Rakesh Patel <emailme@rakeshrpatel.com>
2016-10-07 09:19:47 -0400

    adding media queries

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ $(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            console.log('this is the target', target);
             if (target.length) {
                 $('html, body').animate({
                     scrollTop: target.offset().top
