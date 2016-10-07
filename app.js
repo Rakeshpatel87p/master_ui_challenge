c8497cb842aad7ec9b4e7d142b6847cea7331795
Rakesh Patel <emailme@rakeshrpatel.com>
2016-10-07 07:32:19 -0400

    updates

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(document).scroll(function() {
+        $('.mainNavigationBar')
+        	.css('position', 'fixed')
+            .css('padding-bottom', '0')
+        
+    })
+})
